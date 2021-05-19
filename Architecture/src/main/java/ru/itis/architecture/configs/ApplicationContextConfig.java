32dea0f58e94603ff6a45696f2e404aa1bfac350
“Ainur08” <ajigatin@stud.kpfu.ru>
2021-05-19 23:25:14 +0300

    fixed bugs

--- Architecture/src/main/java/ru/itis/architecture/configs/ApplicationContextConfig.java
+++ Architecture/src/main/java/ru/itis/architecture/configs/ApplicationContextConfig.java
@@ -4,10 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ApplicationContextConfig implements WebMvcConfigurer {
+public class ApplicationContextConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
